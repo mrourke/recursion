1f8f331cc7b3ce5c83e5085c42f0a0499a58bf4e
Matthew Rourke <mrourke@gmail.com>
2015-03-09 22:08:57 -0400

    Misinterpreted JSON syntax, refactoring.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,10 +3,16 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj, subObj) {
-	var objKeys = Object.keys(obj),
+var stringifyJSON2 = function(obj, subObj) {
+	if (obj === null)
+        return String(obj);
+    var objKeys = Object.keys(obj),
 		stringified = '',
 		type = '';
+	if (objKeys.length === 0 && typeof obj !== 'function' && typeof obj !== 'undefined')
+	    return String(obj);
+	if (typeof obj === 'string')
+	    return '"'+obj+'"';
 	for (var x=0;x<objKeys.length;x++) {
 		type = typeof obj[objKeys[x]];
 		if (subObj === 1 && type !== 'function' && type !== undefined) {
@@ -16,7 +22,7 @@ var stringifyJSON = function(obj, subObj) {
 			stringified += null;
 		} else if (type === 'object') {
 			if (Array.isArray(obj[objKeys[x]])) {
-                stringified += stringifyJSON(obj[objKeys[x]]);
+                stringified += '['+stringifyJSON(obj[objKeys[x]])+']';
             } else {
             	stringified += '{' + stringifyJSON(obj[objKeys[x]],1) + '}';
             }
@@ -31,10 +37,28 @@ var stringifyJSON = function(obj, subObj) {
 			stringified += ",";
 		}
 	}
-	if (subObj !== 1) {
+	/*if (subObj !== 1) {
 		return '[' + stringified + ']';
-	} else {
+	} else {*/
 		return stringified;
-	}
+	//}
   // your code goes here
 };
+
+var stringifyJSON = function(obj) {
+	var stringified = '';
+	switch (typeof obj) {
+		case 'number':
+			return String(obj);
+		case 'string':
+			return '"' + obj + '"';
+		case 'boolean':
+			return String(obj);
+		case 'object':
+			if (obj === null) 
+				return 'null';
+			var objKeys = Object.keys(obj);
+			
+	}
+
+}
